1e897ade24d6a58d2433a1b4bc04661182fe216c
Lawal Yusuf Olayinka <94022046+Usersive@users.noreply.github.com>
2024-03-20 09:55:13 +0100

    static

--- horengpt/settings.py
+++ horengpt/settings.py
@@ -49,12 +49,14 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     "whitenoise.middleware.WhiteNoiseMiddleware",
+    # 'django.contrib.staticfiles.middleware.StaticFilesMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    
 ]
 
 ROOT_URLCONF = 'horengpt.urls'
@@ -126,9 +128,9 @@ USE_TZ = True
 
 
 STATIC_URL = '/static/'
-STATIC_ROOT=BASE_DIR /'static'
+# STATIC_ROOT=BASE_DIR /'static'
 
-# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 if not DEBUG:
     STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
